bfcf04a426daab263784fcfb6ed9d024bf172950
Rony Setyawan <rony.setyawan.st@gmail.com>
2021-09-22 11:25:42 +0700

    update wording

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome Telkom Developer Network
+          Welcome Students
         </p>
         <a
           className="App-link"
